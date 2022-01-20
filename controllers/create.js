ebe8b2d55577e5fe7308678c421757e1d5f40783
Parth Sanghavi <sanghaviparth99@gmail.com>
2022-01-20 16:14:23 -0500

    Updated create item quantity validation

--- controllers/create.js
+++ controllers/create.js
@@ -6,45 +6,54 @@ exports.createInventory = ( req, res, next ) => {
 
   res.setHeader(headerContentType, resDataType);
   let requestBody = req.body;
-
-  Items.findOne( { itemCode: requestBody.itemCode} ).then((item)=>{
-    if(!item){
-      Items.create( requestBody )
-      .then( item => {
-        res.statusCode = 201
-        res.json({
-          success : true,
-          message: `${item.itemName} added to the inventory!`,
-          item : item
+  if (Number(requestBody.quantity)<=0){
+    res.statusCode = 400
+    res.json({
+      success : false,
+      message: `Item quantity should be atleast 1.`,
+    })
+  }else{
+    
+    Items.findOne( { itemCode: requestBody.itemCode} ).then((item)=>{
+      if(!item){
+        Items.create( requestBody )
+        .then( item => {
+          res.statusCode = 201
+          res.json({
+            success : true,
+            message: `${item.itemName} added to the inventory!`,
+            item : item
+          })
         })
-      })
-      .catch( err => {
+        .catch( err => {
+          res.statusCode = 400
+          res.json({
+            success : false,
+            errorMessage : err,
+            message: `Sorry, the item could not be added to the inventory! Please try again in some time.`,
+          })
+        })
+      } else{
         res.statusCode = 400
-        res.json({
-          success : false,
-          errorMessage : err,
-          message: `Sorry, the item could not be added to the inventory! Please try again in some time.`,
-        })
-      })
-    } else{
-      res.statusCode = 400
-      if (!item.isDeleted){
-        // if item is already present in the inventory
-        res.json({
-        success : false,
-        message: `Sorry, the item already exists in the inventory!`,
-        })
-      } else {
-        // if item is present in the deleted list
+        if (!item.isDeleted){
+          // if item is already present in the inventory
           res.json({
           success : false,
-          message: `Sorry, the item already exists in the deleted items list of the inventory!`,
-        })
-        /*
-        In future, rather than giving the error, we can add the deleted item back to the 
-        inventory and update it wiith the latest details specified by the user.
-        */
+          message: `Sorry, the item already exists in the inventory!`,
+          })
+        } else {
+          // if item is present in the deleted list
+            res.json({
+            success : false,
+            message: `Sorry, the item already exists in the deleted items list of the inventory!`,
+          })
+          /*
+          In future, rather than giving the error, we can add the deleted item back to the 
+          inventory and update it wiith the latest details specified by the user.
+          */
+        }
       }
-    }
-  })
+    })
+  }
+
 }
