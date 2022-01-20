5256a1107018f0afd51055db8290a6780d433afd
Parth Sanghavi <sanghaviparth99@gmail.com>
2022-01-20 00:09:01 -0500

    Create validation added for unique itemCode.

--- controllers/create.js
+++ controllers/create.js
@@ -6,22 +6,45 @@ exports.createInventory = ( req, res, next ) => {
 
   res.setHeader(headerContentType, resDataType);
   let requestBody = req.body;
-  Items.create( requestBody )
-  .then( item => {
-    res.statusCode = 200
-    res.json({
-      success : true,
-      message: `${item.itemName} added to the inventory!`,
-      item : item
-    })
-  })
-  .catch( err => {
-    res.statusCode = 500
-    res.json({
-      success : false,
-      errorMessage : err,
-      message: `Sorry, the item could not be added to the inventory! Please try again in some time.`,
-    })
-  })
 
+  Items.findOne( { itemCode: requestBody.itemCode} ).then((item)=>{
+    if(!item){
+      Items.create( requestBody )
+      .then( item => {
+        res.statusCode = 201
+        res.json({
+          success : true,
+          message: `${item.itemName} added to the inventory!`,
+          item : item
+        })
+      })
+      .catch( err => {
+        res.statusCode = 400
+        res.json({
+          success : false,
+          errorMessage : err,
+          message: `Sorry, the item could not be added to the inventory! Please try again in some time.`,
+        })
+      })
+    } else{
+      res.statusCode = 400
+      if (!item.isDeleted){
+        // if item is already present in the inventory
+        res.json({
+        success : false,
+        message: `Sorry, the item already exists in the inventory!`,
+        })
+      } else {
+        // if item is present in the deleted list
+          res.json({
+          success : false,
+          message: `Sorry, the item already exists in the deleted items list of the inventory!`,
+        })
+        /*
+        In future, rather than giving the error, we can add the deleted item back to the 
+        inventory and update it wiith the latest details specified by the user.
+        */
+      }
+    }
+  })
 }
